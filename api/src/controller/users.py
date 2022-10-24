ee2515e2b96cae55f2258fa07790b0f4adcc530e
alex <axl3210@rit.edu>
2022-10-24 18:07:02 -0400

    remove user code that is old

--- api/src/controller/users.py
+++ api/src/controller/users.py
@@ -5,30 +5,9 @@ from data.users import createUser, getUser
 
 class Users(Resource):
     def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument("firstName", type=str)
-        parser.add_argument("lastName", type=str)
-        parser.add_argument("pronouns", type=str)
-
-        args = parser.parse_args()
-        firstName = args["firstName"]
-        lastName = args["lastName"]
-        pronouns = args["pronouns"]
-
-        is_created = createUser(firstName, lastName, pronouns)
-
-        if is_created is False:
-            return "User Not Created", 400
-        return "User Successfully Created", 200
+        return "Not Implemented Yet", 415
 
     def get(self):
-        user_id = request.args.get('id')
-
-        userInfo = getUser(user_id)
-
-        if userInfo is None:
-            return "User Not Found", 400
-
-        return {"user": userInfo}, 200
+        return "Not Implemented Yet", 415
 
 
