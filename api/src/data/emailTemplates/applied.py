8254b382b4002cbfbb74eed9fa1e5aac4a759faa
Hallie <70453087+ha1lie@users.noreply.github.com>
2023-01-06 16:37:23 -0500

    Stylize acceptance email
    Voila, I'd say that looks pretty good

--- api/src/data/emailTemplates/applied.py
+++ api/src/data/emailTemplates/applied.py
@@ -1,18 +1,25 @@
 def getAppliedEmail(firstName, lastName):
-    return f'<div>' \
-           f'   <div>' \
-           f'       WiCHacks Header' \
-           f'   </div>' \
-           f'   <div>' \
-           f'       <h3>Thank you for applying {firstName} {lastName}!</h3>' \
-           f'       <p>' \
-           f'           Your application has been submitted and will be reviewed by a WiCHacks Organizer' \
-           f'       </p>' \
-           f'   </div>' \
-           f'   <div>' \
-           f'       WiCHacks Footer' \
-           f'   </div>' \
-           f'</div>'
+    try:
+        appliedEmail = open('./src/data/emailTemplates/applied.html', 'r')
+        contents = appliedEmail.read()
+        appliedEmail.close()
+        contents = contents.replace("first_name", firstName).replace("last_name", lastName)
+        return contents
+    except:    
+        return f'<div>' \
+            f'   <div>' \
+            f'       WiCHacks Header' \
+            f'   </div>' \
+            f'   <div>' \
+            f'       <h3>Thank you for applying {firstName} {lastName}!</h3>' \
+            f'       <p>' \
+            f'           Your application has been submitted and will be reviewed by a WiCHacks Organizer' \
+            f'       </p>' \
+            f'   </div>' \
+            f'   <div>' \
+            f'       WiCHacks Footer' \
+            f'   </div>' \
+            f'</div>'
 
 
 def getAppliedSubjectLine() -> str:
