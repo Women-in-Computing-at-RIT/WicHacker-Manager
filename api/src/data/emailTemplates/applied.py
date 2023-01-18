6baa905260b46512018e37e1485c5965e42c397b
alex <axl3210@rit.edu>
2023-01-17 22:30:44 -0500

    update default email for applications

--- api/src/data/emailTemplates/applied.py
+++ api/src/data/emailTemplates/applied.py
@@ -8,16 +8,10 @@ def getAppliedEmail(firstName, lastName):
     except:    
         return f'<div>' \
             f'   <div>' \
-            f'       WiCHacks Header' \
-            f'   </div>' \
-            f'   <div>' \
             f'       <h3>Thank you for applying {firstName} {lastName}!</h3>' \
             f'       <p>' \
             f'           Your application has been submitted and will be reviewed by a WiCHacks Organizer' \
             f'       </p>' \
-            f'   </div>' \
-            f'   <div>' \
-            f'       WiCHacks Footer' \
             f'   </div>' \
             f'</div>'
 
